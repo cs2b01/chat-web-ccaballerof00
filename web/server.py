bd021070678ef603da440c87e073de1352871633
Cristian <cristian.caballero@utec.edu.pe>
2019-06-02 22:31:46 -0500

    Ya se pueden crear mensajes

--- web/server.py
+++ web/server.py
@@ -38,14 +38,6 @@ def get_user(id):
     message = { 'status': 404, 'message': 'Not Found'}
     return Response(message, status=404, mimetype='application/json')
 
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
-
 
 
 @app.route('/users', methods = ['POST'])
@@ -83,6 +75,60 @@ def update_user():
     session.add(user)
     session.commit()
     return 'Updated User'
+
+@app.route('/messages', methods = ['GET'])
+def get_messages():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.Message)
+    data = []
+    for message in dbResponse:
+        data.append(message)
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+@app.route('/messages', methods = ['POST'])
+def create_message():
+    t = json.loads(request.form['values'])
+    message = entities.Message(
+        content=t['content'],
+        user_from_id=t['user_from_id'],
+        user_to_id=t['user_to_id']
+    )
+    session = db.getSession(engine)
+    session.add(message)
+    session.commit()
+    return "Message Created"
+
+@app.route('/messages', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    messages = session.query(entities.Message).filter(entities.Message.id == id)
+    for message in messages:
+        session.delete(message)
+    session.commit()
+    return "Message Deleted"
+
+@app.route('/create_test_messages', methods = ['GET'])
+def create_test_messages():
+    db_session = db.getSession(engine)
+    message = entities.Message(content="Test message", user_from_id=1, user_to_id=2)
+    db_session.add(message)
+    db_session.commit()
+    return "Test message created"
+
+@app.route('/authenticate',methods=["POST"])
+def authenticate():
+    #1. Get data from request
+    username = request.form['username']
+    password = request.form['password']
+
+    #2. Look in database
+    db_session = db.getSession(engine)
+    try:
+            users= db_session.query(entities.User).filter(entities.User.username==username).filter(entities.User.password==password).one()
+            return render_template("success.html")
+    except Exception:
+            return render_template("Fail.html")
 
 if __name__ == '__main__':
     app.secret_key = ".."
